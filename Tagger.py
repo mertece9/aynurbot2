2a06f4dcd9be1e39751982a445342ca026a1d82c
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-05-01 09:42:17 +0400

    update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -452,7 +452,7 @@ async def mentionall(event):
       if event.chat_id not in anlik_calisan:
         await event.respond("** Etiket işlemi başarıyla durduruldu❌**")
         return
-      if usrnum == 3:
+      if usrnum == 5:
         await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
         await asyncio.sleep(2)
         usrnum = 0
@@ -470,7 +470,7 @@ async def mentionall(event):
       if event.chat_id not in anlik_calisan:
         await event.respond("Işlem Başarıyla Durduruldu\n\n**Buda sizin reklamınız ola bilir @ben_aynurbot**❌")
         return
-      if usrnum == 3:
+      if usrnum == 5:
         await client.send_message(event.chat_id, usrtxt, reply_to=msg)
         await asyncio.sleep(2)
         usrnum = 0
